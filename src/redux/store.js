ca1b0062b21821bb6245cd4335386eefc2371319
tareqnmd <tareqnmd@gmail.com>
2023-02-19 08:47:17 +0600

    middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,17 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './rootReducer';
 
-const store = createStore(rootReducer);
+const myLogger = (store) => (next) => (action) => {
+	console.log('action', JSON.stringify(action));
+	console.log('Before', JSON.stringify(store.getState()));
+
+	const upcomingState = [action].reduce(rootReducer, store.getState());
+
+	console.log('After', JSON.stringify(upcomingState));
+
+	return next(action);
+};
+
+const store = createStore(rootReducer, applyMiddleware(myLogger));
 
 export default store;
