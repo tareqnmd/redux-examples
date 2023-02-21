9edcd573285993a2bb7b0e49bf9e2dbd26128834
Mohammad Tareq <mohammad.tareq@apsissolutions.com>
2023-02-21 21:30:21 +0600

    middleware up with devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
-import myLogger from './middlewares/myLogger';
+import myLogger from './middleware/myLogger';
 import rootReducer from './rootReducer';
 
-const store = createStore(rootReducer, applyMiddleware(myLogger, logger));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(myLogger, logger)));
 
 export default store;
