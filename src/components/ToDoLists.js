8dbf4c084f1e2cc4604594bc1d09fda5f365c1a1
Mohammad Tareq <mohammad.tareq@apsissolutions.com>
2023-02-22 15:22:32 +0600

    TODO APP

--- src/components/ToDoLists.js
+++ src/components/ToDoLists.js
@@ -1,13 +1,35 @@
 import { useSelector } from 'react-redux';
 import ToDo from './ToDo';
 
+const filterByStatus = (todo, status) => {
+	switch (status) {
+		case 'Complete':
+			return todo.completed;
+		case 'Incomplete':
+			return !todo.completed;
+		default:
+			return true;
+	}
+};
+const filterByColor = (todo, colors) => {
+	if (colors.length > 0) {
+		return colors.includes(todo?.color);
+	}
+	return true;
+};
+
 const ToDoLists = () => {
 	const todos = useSelector((state) => state.todos);
+	const filters = useSelector((state) => state.filters);
+	const { status, colors } = filters;
 	return (
 		<div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
-			{todos.map((todo) => (
-				<ToDo key={todo.id} todo={todo} />
-			))}
+			{todos
+				.filter((todo) => filterByStatus(todo, status))
+				.filter((todo) => filterByColor(todo, colors))
+				.map((todo) => (
+					<ToDo key={todo.id} todo={todo} />
+				))}
 		</div>
 	);
 };
