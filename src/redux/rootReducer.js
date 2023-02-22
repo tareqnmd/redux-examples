cb3f29538194ca84613ff30134b670ef5908f78f
Mohammad Tareq <mohammad.tareq@apsissolutions.com>
2023-02-22 13:09:01 +0600

    up

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,6 +2,6 @@ import { combineReducers } from 'redux';
 import filterReducer from './filters/filterReducer';
 import todoReducer from './todos/todoReducer';
 
-const rootReducer = combineReducers({ todo: todoReducer, filter: filterReducer });
+const rootReducer = combineReducers({ todos: todoReducer, filters: filterReducer });
 
 export default rootReducer;
