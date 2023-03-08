66ee8d82298f45465c96df7e583ba657ee66e4ab
tareqnmd <tareqnmd@gmail.com>
2023-03-08 12:46:59 +0600

    up

--- src/components/tags/Tag.js
+++ src/components/tags/Tag.js
@@ -1,11 +1,11 @@
-const Tag = () => {
+const Tag = ({ tag }) => {
 	return (
 		<div
 			className={`${
 				true ? 'bg-blue-100 text-blue-600' : 'bg-blue-600 text-white'
 			} px-4 py-1 rounded-full cursor-pointer`}
 		>
-			react
+			{tag?.title}
 		</div>
 	);
 };
---
+++ src/features/tags/tagsSlice.js
@@ -0,0 +1,38 @@
+import { getTags } from './tagsApi';
+const { createAsyncThunk, createSlice } = require('@reduxjs/toolkit');
+
+const initialState = {
+	tags: [],
+	isLoading: false,
+	isError: false,
+	error: '',
+};
+
+export const fetchTags = createAsyncThunk('tags/fetchTags', async () => {
+	const tags = await getTags();
+	return tags;
+});
+
+const tagsSlice = createSlice({
+	name: 'tags',
+	initialState,
+	extraReducers: (builder) => {
+		builder
+			.addCase(fetchTags.pending, (state) => {
+				state.isLoading = true;
+				state.isError = false;
+			})
+			.addCase(fetchTags.fulfilled, (state, action) => {
+				state.isLoading = false;
+				state.tags = action.payload;
+			})
+			.addCase(fetchTags.rejected, (state, action) => {
+				state.tags = [];
+				state.isLoading = false;
+				state.isError = true;
+				state.error = action.error?.message;
+			});
+	},
+});
+
+export default tagsSlice.reducer;
