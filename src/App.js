82f70529b3ceb33196f2b4e39d57b0d7991d6192
tareqnmd <tareqnmd@gmail.com>
2023-03-08 10:55:33 +0600

    router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
 import Home from './pages/Home';
 import Video from './pages/Video';
 
 export default function App() {
 	return (
-		<>
-			<Home />
-			<Video />
-		</>
+		<Router>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />}></Route>
+				<Route path="/videos/:videoId" element={<Video />}></Route>
+			</Routes>
+			<Footer />
+		</Router>
 	);
 }
