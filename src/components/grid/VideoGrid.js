ad37c48d873affc422d67f11d3f895715f8084a4
tareqnmd <tareqnmd@gmail.com>
2023-03-08 12:09:07 +0600

    up

--- src/components/grid/VideoGrid.js
+++ src/components/grid/VideoGrid.js
@@ -1,12 +1,22 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchVideos } from '../../features/videos/videosSlice';
 import VideoGridItem from './VideoGridItem';
 
 const VideoGrid = () => {
+	const { videos, isError, error } = useSelector((state) => state.videos);
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(fetchVideos());
+	}, [dispatch]);
 	return (
 		<section className="pt-12">
 			<section className="pt-12">
 				<div className="grid grid-cols-12 gap-4 max-w-7xl mx-auto px-5 lg:px-0 min-h-[300px]">
-					<VideoGridItem />
-					{/* <div className="col-span-12">some error happened</div> */}
+					{videos.map((video) => (
+						<VideoGridItem key={video.id} video={video} />
+					))}
+					{isError && <div className="col-span-12">{error}</div>}
 				</div>
 			</section>
 		</section>
--- src/components/grid/VideoGridItem.js
+++ src/components/grid/VideoGridItem.js
@@ -1,16 +1,13 @@
 import { Link, NavLink } from 'react-router-dom';
 
-const VideoGridItem = () => {
+const VideoGridItem = ({ video }) => {
+	const { avatar, thumbnail, author, title, views, date, id } = video;
 	return (
 		<div className="col-span-12 sm:col-span-6 md:col-span-3 duration-300 hover:scale-[1.03]">
 			<div className="w-full flex flex-col">
 				<div className="relative">
-					<Link to={`/videos/${1}`}>
-						<img
-							src="https://i3.ytimg.com/vi/6O4s7v28nlw/maxresdefault.jpg"
-							className="w-full h-auto"
-							alt="Some video title"
-						/>
+					<Link to={`/videos/${id}`}>
+						<img src={thumbnail} className="w-full h-auto" alt="Some video title" />
 					</Link>
 
 					<p className="absolute right-2 bottom-2 bg-gray-900 text-gray-100 text-xs px-1 py">
@@ -19,22 +16,20 @@ const VideoGridItem = () => {
 				</div>
 
 				<div className="flex flex-row mt-2 gap-2">
-					<NavLink to={`/videos/${1}`} className="shrink-0">
-						<img
-							src="https://avatars.githubusercontent.com/u/73503432?v=4"
-							className="rounded-full h-6 w-6"
-							alt="Learn with Sumit"
-						/>
+					<NavLink to={`/videos/${id}`} className="shrink-0">
+						<img src={avatar} className="rounded-full h-6 w-6" alt="Learn with Sumit" />
 					</NavLink>
 
 					<div className="flex flex-col">
-						<Link to={`/videos/${1}`}>
-							<p className="text-slate-900 text-sm font-semibold">Video title</p>
+						<Link to={`/videos/${id}`}>
+							<p className="text-slate-900 text-sm font-semibold">{title}</p>
 						</Link>
 						<NavLink to="/" className="text-gray-400 text-xs mt-2 hover:text-gray-600">
-							Learn with Sumit
+							{author}
 						</NavLink>
-						<p className="text-gray-400 text-xs mt-1">200 views . May 3, 2022</p>
+						<p className="text-gray-400 text-xs mt-1">
+							{views} . {date}
+						</p>
 					</div>
 				</div>
 			</div>
