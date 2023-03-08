80cf3b0467d10c0148ffb39ee24319a9701842e7
tareqnmd <tareqnmd@gmail.com>
2023-03-08 17:35:15 +0600

    up

--- src/features/videos/videosApi.js
+++ src/features/videos/videosApi.js
@@ -1,9 +1,9 @@
 import axios from '../../utils/axios';
 
 export const getVideos = async (tags, search) => {
-	let queryString = '?';
-	queryString += tags?.length > 0 ? tags.map((tag) => `tags_like=${tag}`).join('&') : '';
-	queryString += search?.length > 0 ? `&q=${search}` : '';
-	const response = await axios.get(`/videos${queryString}`);
+	const queryString =
+		`${tags?.length > 0 ? tags.map((tag) => `tags_like=${tag}`).join('&') : ''}` +
+		`${search?.length > 0 ? `&q=${search}` : ''}`;
+	const response = await axios.get(`/videos${queryString.length > 0 ? `?${queryString}` : ''}`);
 	return response.data;
 };
