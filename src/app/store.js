9628d393d74e72d2b13b67b609d923e9d47bbea1
tareqnmd <tareqnmd@gmail.com>
2023-03-08 12:56:54 +0600

    up

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import tagsReducer from '../features/tags/tagsSlice';
+import videoReducer from '../features/videos/videoSlice';
 import videosReducer from '../features/videos/videosSlice';
 
 export const store = configureStore({
 	reducer: {
 		videos: videosReducer,
+		video: videoReducer,
 		tags: tagsReducer,
 	},
 });
