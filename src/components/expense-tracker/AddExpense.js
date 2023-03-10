9ef6a2fb0c2a7201223ece6016c59df38b0fe8fa
tareqnmd <tareqnmd@gmail.com>
2023-03-10 18:02:39 +0600

    up

--- src/components/expense-tracker/AddExpense.js
+++ src/components/expense-tracker/AddExpense.js
@@ -4,24 +4,30 @@ const AddExpense = () => {
 			<h3>Add new transaction</h3>
 
 			<div className="form-group">
-				<label for="transaction_name">Name</label>
+				<label htmlFor="transaction_name">Name</label>
 				<input type="text" name="transaction_name" placeholder="My Salary" />
 			</div>
 
 			<div className="form-group radio">
-				<label for="transaction_type">Type</label>
+				<label htmlFor="transaction_type">Type</label>
 				<div className="radio_group">
-					<input type="radio" value="income" name="transaction_type" checked />
-					<label for="transaction_type">Income</label>
+					<input onChange={() => {}} type="radio" value="income" name="transaction_type" checked />
+					<label htmlFor="transaction_type">Income</label>
 				</div>
 				<div className="radio_group">
-					<input type="radio" value="expense" name="transaction_type" placeholder="Expense" />
-					<label for="transaction_type">Expense</label>
+					<input
+						onChange={() => {}}
+						type="radio"
+						value="expense"
+						name="transaction_type"
+						placeholder="Expense"
+					/>
+					<label htmlFor="transaction_type">Expense</label>
 				</div>
 			</div>
 
 			<div className="form-group">
-				<label for="transaction_amount">Amount</label>
+				<label htmlFor="transaction_amount">Amount</label>
 				<input type="number" placeholder="300" name="transaction_amount" />
 			</div>
 
