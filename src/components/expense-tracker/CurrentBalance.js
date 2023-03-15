0f3055e6b597959f1a0626c07bc1329e933ad4e3
tareqnmd <tareqnmd@gmail.com>
2023-03-15 08:41:05 +0600

    current balance

--- src/components/expense-tracker/CurrentBalance.js
+++ src/components/expense-tracker/CurrentBalance.js
@@ -1,10 +1,26 @@
+import { useSelector } from 'react-redux';
+
+const getBalance = (transactions) => {
+	return transactions.reduce(
+		(acc, transaction) =>
+			acc +
+			(transaction.type === 'income'
+				? transaction.amount
+				: transaction.type === 'expense'
+				? -transaction.amount
+				: 0),
+		0
+	);
+};
+
 const CurrentBalance = () => {
+	const { transactions } = useSelector((state) => state.transaction);
 	return (
 		<div className="top_card">
 			<p>Your Current Balance</p>
 			<h3>
 				<span>৳</span>
-				<span>10500</span>
+				<span>{getBalance(transactions)}</span>
 			</h3>
 		</div>
 	);
