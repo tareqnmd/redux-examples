bbea72fc960aa0c93920a341d4cf7ca27d265758
tareqnmd <tareqnmd@gmail.com>
2023-03-17 23:17:07 +0600

    video api

--- src/components/videos/Videos.js
+++ src/components/videos/Videos.js
@@ -1,15 +1,30 @@
-import Video from "./Video";
+import { useGetVideosQuery } from '../../features/api/apiSlice';
+import Error from '../ui/Error';
+import VideoLoader from '../ui/loaders/VideoLoader';
+import Video from './Video';
 
 export default function Videos() {
-    return (
-        <>
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-        </>
-    );
+	const { data: videos, isLoading, isError } = useGetVideosQuery();
+
+	let content = null;
+	if (isLoading) {
+		content = (
+			<>
+				<VideoLoader />
+				<VideoLoader />
+				<VideoLoader />
+				<VideoLoader />
+			</>
+		);
+	}
+	if (!isLoading && isError) {
+		content = <Error message="There was an error" />;
+	}
+	if (!isLoading && !isError && videos?.length === 0) {
+		content = <Error message="No Videos Found" />;
+	}
+	if (!isLoading && !isError && videos?.length > 0) {
+		content = videos.map((video) => <Video video={video} />);
+	}
+	return content;
 }
