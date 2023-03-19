7e2b5167fa43041a3710fb36e30a56368e5e7f6f
tareqnmd <tareqnmd@gmail.com>
2023-03-19 23:07:14 +0600

    add href

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Conversation from "./pages/Conversation";
-import Inbox from "./pages/Inbox";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Conversation from './pages/Conversation';
+import Inbox from './pages/Inbox';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/inbox" element={<Conversation />} />
-                <Route path="/inbox/:id" element={<Inbox />} />
-            </Routes>
-        </Router>
-    );
+	return (
+		<Router>
+			<Routes>
+				<Route
+					path="/"
+					element={<Login />}
+				/>
+				<Route
+					path="/register"
+					element={<Register />}
+				/>
+				<Route
+					path="/inbox"
+					element={<Conversation />}
+				/>
+				<Route
+					path="/inbox/:id"
+					element={<Inbox />}
+				/>
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
--- src/components/inbox/chatbody/Messages.js
+++ src/components/inbox/chatbody/Messages.js
@@ -1,28 +1,82 @@
-import Message from "./Message";
+import Message from './Message';
 
 export default function Messages() {
-    return (
-        <div className="relative w-full h-[calc(100vh_-_197px)] p-6 overflow-y-auto flex flex-col-reverse">
-            <ul className="space-y-2">
-                <Message justify="start" message="Hjdjd" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-                <Message justify="start" message="Hi" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-                <Message justify="start" message="Hi" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-                <Message justify="start" message="Hi" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-                <Message justify="start" message="Hi" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-                <Message justify="start" message="Hi" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-            </ul>
-        </div>
-    );
+	return (
+		<div className="relative w-full h-[calc(100vh_-_197px)] p-6 overflow-y-auto flex flex-col-reverse">
+			<ul className="space-y-2">
+				<Message
+					justify="start"
+					message="Hello"
+				/>
+				<Message
+					justify="start"
+					message="How are you?"
+				/>
+				<Message
+					justify="end"
+					message="I am fine what about you?"
+				/>
+				<Message
+					justify="start"
+					message="Hi"
+				/>
+				<Message
+					justify="start"
+					message="How are you?"
+				/>
+				<Message
+					justify="end"
+					message="I am fine what about you?"
+				/>
+				<Message
+					justify="start"
+					message="Hi"
+				/>
+				<Message
+					justify="start"
+					message="How are you?"
+				/>
+				<Message
+					justify="end"
+					message="I am fine what about you?"
+				/>
+				<Message
+					justify="start"
+					message="Hi"
+				/>
+				<Message
+					justify="start"
+					message="How are you?"
+				/>
+				<Message
+					justify="end"
+					message="I am fine what about you?"
+				/>
+				<Message
+					justify="start"
+					message="Hi"
+				/>
+				<Message
+					justify="start"
+					message="How are you?"
+				/>
+				<Message
+					justify="end"
+					message="I am fine what about you?"
+				/>
+				<Message
+					justify="start"
+					message="Hi"
+				/>
+				<Message
+					justify="start"
+					message="How are you?"
+				/>
+				<Message
+					justify="end"
+					message="I am fine what about you?"
+				/>
+			</ul>
+		</div>
+	);
 }
