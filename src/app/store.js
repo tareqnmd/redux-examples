f454345169c8fe0cf27ce428ad18ca777a55547b
tareqnmd <tareqnmd@gmail.com>
2023-03-20 08:44:43 +0600

    slice added

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { apiSlice } from '../features/api/apiSlice';
+import authReducer from '../features/auth/authSlice';
+import conversationsReducer from '../features/conversations/conversationsSlice';
+import messagesReducer from '../features/messages/messagesSlice';
 
 export const store = configureStore({
-	reducer: {},
+	reducer: {
+		[apiSlice.reducerPath]: apiSlice.reducer,
+		auth: authReducer,
+		messages: messagesReducer,
+		conversations: conversationsReducer,
+	},
+	devTools: process.env.NODE_ENV !== 'production',
+	middleware: (getDefaultMiddlewares) => getDefaultMiddlewares().concat(apiSlice.middleware),
 });
