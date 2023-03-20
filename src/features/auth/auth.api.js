8fc3c9481b39f22928bce8c7f34607cc67d1c459
tareqnmd <tareqnmd@gmail.com>
2023-03-20 22:58:54 +0600

    authslice

--- src/features/auth/auth.api.js
+++ src/features/auth/auth.api.js
@@ -1,5 +1,45 @@
 import { apiSlice } from '../api/apiSlice';
+import { userLoggedIn } from './authSlice';
 
 export const authApi = apiSlice.injectEndpoints({
-	endpoints: (builder) => {},
+	endpoints: (builder) => ({
+		register: builder.mutation({
+			query: (data) => ({
+				url: '/register',
+				method: 'POST',
+				body: data,
+			}),
+			async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+				try {
+					const result = await queryFulfilled;
+					const data = {
+						accessToken: result.data.accessToken,
+						user: result.data.user,
+					};
+					localStorage.setItem('auth', JSON.stringify(data));
+					dispatch(userLoggedIn(data));
+				} catch (error) {}
+			},
+		}),
+		login: builder.mutation({
+			query: (data) => ({
+				url: '/login',
+				method: 'POST',
+				body: data,
+			}),
+			async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+				try {
+					const result = await queryFulfilled;
+					const data = {
+						accessToken: result.data.accessToken,
+						user: result.data.user,
+					};
+					localStorage.setItem('auth', JSON.stringify(data));
+					dispatch(userLoggedIn(data));
+				} catch (error) {}
+			},
+		}),
+	}),
 });
+
+export const { useRegisterMutation, useLoginMutation } = authApi;
