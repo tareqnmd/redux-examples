b597060a16d30adcaa81c0c5fe673acab38aff37
tareqnmd <tareqnmd@gmail.com>
2023-03-22 08:09:22 +0600

    api

--- src/features/messages/messages.api.js
+++ src/features/messages/messages.api.js
@@ -4,7 +4,7 @@ export const messagesApi = apiSlice.injectEndpoints({
 	endpoints: (builder) => ({
 		getMessages: builder.query({
 			query: (id) => ({
-				url: `/messages?conversationId=${id}&_sort=timestamp&_order=desc&_page=1&_limit=${process.env.REACT_APP_MESSAGES_PER_PAGE}`,
+				url: `/messages?conversationId=${id}&_sort=timestamp&_order=asc&_page=1&_limit=${process.env.REACT_APP_MESSAGES_PER_PAGE}`,
 			}),
 		}),
 	}),
