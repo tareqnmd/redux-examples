cfd187842dc81c408c33df916acd597a32934da9
tareqnmd <tareqnmd@gmail.com>
2023-03-22 08:27:18 +0600

    up

--- src/components/inbox/chatbody/ChatHead.js
+++ src/components/inbox/chatbody/ChatHead.js
@@ -1,12 +1,18 @@
-export default function ChatHead({ avatar, name }) {
+import gravatarUrl from 'gravatar-url';
+import { useSelector } from 'react-redux';
+
+export default function ChatHead({ message }) {
+	const { sender, receiver } = message;
+	const { user } = useSelector((state) => state.auth) || {};
+	const partner = sender?.email === user?.email ? receiver : sender;
 	return (
 		<div className="relative flex items-center p-3 border-b border-gray-300">
 			<img
 				className="object-cover w-10 h-10 rounded-full"
-				src={avatar}
-				alt={name}
+				src={gravatarUrl(partner.email)}
+				alt={partner.name}
 			/>
-			<span className="block ml-2 font-bold text-gray-600">{name}</span>
+			<span className="block ml-2 font-bold text-gray-600">{partner.name}</span>
 		</div>
 	);
 }
