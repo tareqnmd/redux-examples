1ba4fabcc8498985e1d45307bad6b87e259d0540
tareqnmd <tareqnmd@gmail.com>
2023-03-22 07:34:03 +0600

    conversation

--- src/components/inbox/ChatIItems.js
+++ src/components/inbox/ChatIItems.js
@@ -1,28 +1,42 @@
+import { formatDistanceToNow } from 'date-fns';
+import gravatarUrl from 'gravatar-url';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { useGetConversationsQuery } from '../../features/conversations/conversations.api';
+import getPartnerInfo from '../../utils/getPartnerInfo';
+import Error from '../ui/Error';
 import ChatItem from './ChatItem';
-
 export default function ChatItems() {
-	return (
-		<ul>
-			<li>
-				<ChatItem
-					avatar="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg"
-					name="Saad Hasan"
-					lastMessage="bye"
-					lastTime="25 minutes"
-				/>
-				<ChatItem
-					avatar="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg"
-					name="Sumit Saha"
-					lastMessage="will talk to you later"
-					lastTime="10 minutes"
-				/>
-				<ChatItem
-					avatar="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg"
-					name="Mehedi Hasan"
-					lastMessage="thanks for your support"
-					lastTime="15 minutes"
-				/>
+	const { user } = useSelector((state) => state.auth);
+	const { data: conversations, isLoading, isError, error } = useGetConversationsQuery(user?.email);
+	let content = null;
+	if (isLoading) {
+		content = <li className="m-2 text-center">Loading...</li>;
+	} else if (!isLoading && isError) {
+		content = (
+			<li className="m-2 text-center">
+				<Error message={error.data} />
 			</li>
-		</ul>
-	);
+		);
+	} else if (!isLoading && !isError && conversations?.length === 0) {
+		content = <li className="m-2 text-center">No Conversations Found</li>;
+	} else if (!isLoading && !isError && conversations?.length > 0) {
+		content = conversations?.map((conversation) => {
+			const { id, message, timestamp } = conversation;
+			const { name, email } = getPartnerInfo(conversation?.users, user.email);
+			return (
+				<li key={conversation?.id}>
+					<Link to={`/inbox/${id}`}>
+						<ChatItem
+							avatar={gravatarUrl(email, { size: 80 })}
+							name={name}
+							lastMessage={message}
+							lastTime={formatDistanceToNow(timestamp)}
+						/>
+					</Link>
+				</li>
+			);
+		});
+	}
+	return <ul>{content}</ul>;
 }
--- src/components/inbox/ChatItem.js
+++ src/components/inbox/ChatItem.js
@@ -1,11 +1,6 @@
-import { Link } from 'react-router-dom';
-
 export default function ChatItem({ avatar, name, lastMessage, lastTime }) {
 	return (
-		<Link
-			className="flex items-center px-3 py-2 text-sm transition duration-150 ease-in-out border-b border-gray-300 cursor-pointer hover:bg-gray-100 focus:outline-none"
-			to="/"
-		>
+		<div className="flex items-center px-3 py-2 text-sm transition duration-150 ease-in-out border-b border-gray-300 cursor-pointer hover:bg-gray-100 focus:outline-none">
 			<img
 				className="object-cover w-10 h-10 rounded-full"
 				src={avatar}
@@ -18,6 +13,6 @@ export default function ChatItem({ avatar, name, lastMessage, lastTime }) {
 				</div>
 				<span className="block ml-2 text-sm text-gray-600">{lastMessage}</span>
 			</div>
-		</Link>
+		</div>
 	);
 }
