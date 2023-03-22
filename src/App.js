bb7bad93f68ae7748d92f3f58da420a8f79723e0
tareqnmd <tareqnmd@gmail.com>
2023-03-22 06:20:08 +0600

    login logout

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import useAuthCheck from './hooks/useAuthCheck';
 import Conversation from './pages/Conversation';
 import Inbox from './pages/Inbox';
 import Login from './pages/Login';
+import PrivateRoute from './pages/PrivateRoute';
+import PublicRoute from './pages/PublicRoute';
 import Register from './pages/Register';
 
 function App() {
-	return (
+	const authChecked = useAuthCheck();
+	return !authChecked ? (
+		<div>Checking Authentication</div>
+	) : (
 		<Router>
 			<Routes>
 				<Route
 					path="/"
-					element={<Login />}
+					element={
+						<PublicRoute>
+							<Login />
+						</PublicRoute>
+					}
 				/>
 				<Route
 					path="/register"
-					element={<Register />}
+					element={
+						<PublicRoute>
+							<Register />
+						</PublicRoute>
+					}
 				/>
 				<Route
 					path="/inbox"
-					element={<Conversation />}
+					element={
+						<PrivateRoute>
+							<Conversation />
+						</PrivateRoute>
+					}
 				/>
 				<Route
 					path="/inbox/:id"
-					element={<Inbox />}
+					element={
+						<PrivateRoute>
+							<Inbox />
+						</PrivateRoute>
+					}
 				/>
 			</Routes>
 		</Router>
---
+++ src/pages/PublicRoute.js
@@ -0,0 +1,9 @@
+import { Navigate } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
+
+const PublicRoute = ({ children }) => {
+	const isLoggedIn = useAuth();
+	return !isLoggedIn ? children : <Navigate to="/inbox" />;
+};
+
+export default PublicRoute;
